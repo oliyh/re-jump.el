9cc28af11c8cafc221a0ea7615117bff3c5cc66a
Oliver Hine <git@oliy.co.uk>
2017-12-12 18:52:30 +0000

    Keeping emacs happy

--- re-jump.el
+++ re-jump.el
@@ -1,3 +1,29 @@
+;;; re-jump.el --- emacs navigation for re-frame projects
+
+;; Copyright © 2017 Oliver Hine
+;;
+;; Author: Oliver Hine
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Jump to registrations of re-frame subscriptions, event handlers and fx
+
+;;; Code:
 (require 'cider-util)
 (require 'cider-resolve)
 (require 'cider-client)
