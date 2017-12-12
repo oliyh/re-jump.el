833dfa819e48ac18c7a8d0e5c47204dac973beba
Oliver Hine <git@oliy.co.uk>
2017-12-12 18:49:13 +0000

    Using cider-find-ns on recommendation from bbatsov, more reliable file finding

--- re-jump.el
+++ re-jump.el
@@ -14,19 +14,15 @@
          (kw-ns (if ns-qualifier
                     (cider-resolve-alias (cider-current-ns) ns-qualifier)
                   (cider-current-ns)))
-         (target-file (concat (clojure-project-dir) (cider-sync-request:ns-path kw-ns)))
-         (kw-to-find (concat "::" (replace-regexp-in-string "^:+\\(.+/\\)?" "" kw)))
-         (buffer (cider--find-buffer-for-file target-file)))
+         (kw-to-find (concat "::" (replace-regexp-in-string "^:+\\(.+/\\)?" "" kw))))
 
     (when (and ns-qualifier (string= kw-ns (cider-current-ns)))
       (error "Could not resolve alias \"%s\" in %s" ns-qualifier (cider-current-ns)))
-        
-    (if buffer
-        (progn (cider-jump-to buffer)
-               (search-forward-regexp (concat "reg-[a-zA-Z-]*[ \\\n]+" kw-to-find) nil 'noerror))
-      (error "Could not open a buffer for %s" target-file))))
-      
+
+    (progn (cider-find-ns "-" kw-ns)
+           (search-forward-regexp (concat "reg-[a-zA-Z-]*[ \\\n]+" kw-to-find) nil 'noerror))))
+
 (global-set-key (kbd "M->") 're-frame-jump-to-reg)
-      
+
 (provide 're-jump)
 ;;; re-jump.el ends here
